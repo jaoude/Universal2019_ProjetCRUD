8d42072e43c00e93048c9497ea2c0625e199836d
MikeMajdalani <michel.majdalani@net.usj.edu.lb>
2019-10-11 08:30:44 +0300

    Added code for the upload file

--- MultiGrain.Client/src/app/app-routing.module.ts
+++ MultiGrain.Client/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import {NgModule} from '@angular/core';
 import { Routes, RouterModule}
 from '@angular/router';
 import {EmployeesComponent} from './employees/employees.component';
+import { UploadsComponent } from './uploads/uploads.component';
 
 const routes : Routes = [
   { path: 'students', component: EmployeesComponent },
-  { path: 'Institutions', component: EmployeesComponent }
+  { path: 'Institutions', component: UploadsComponent },
+  { path: 'FileDocuments', component: UploadsComponent }
 ];
 
 @NgModule ({
--- MultiGrain.Client/src/app/app.module.ts
+++ MultiGrain.Client/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { FormsModule } from "@angular/forms";
 import { HttpClientModule } from "@angular/common/http";
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ToastrModule } from 'ngx-toastr';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { EmployeesComponent } from './employees/employees.component';
@@ -11,13 +12,17 @@ import { EmployeeComponent } from './employees/employee/employee.component';
 import { EmployeeListComponent } from './employees/employee-list/employee-list.component';
 import { EmployeeService } from './shared/employee.service';
 import { AppRoutingModule, routingComponent} from './app-routing.module';
+import { UploadsComponent } from './uploads/uploads.component';
+import { FileUploadService } from './upload_service/file-upload.service';
+
 @NgModule({
   declarations: [
     AppComponent,
     EmployeesComponent,
     EmployeeComponent,
     EmployeeListComponent,
-    routingComponent
+    routingComponent,
+    UploadsComponent
   ],
   imports: [
     BrowserModule,
@@ -25,9 +30,10 @@ import { AppRoutingModule, routingComponent} from './app-routing.module';
     HttpClientModule,
     BrowserAnimationsModule,
     AppRoutingModule,
+    ReactiveFormsModule,
     ToastrModule.forRoot()
   ],
-  providers: [EmployeeService],
+  providers: [EmployeeService,FileUploadService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
