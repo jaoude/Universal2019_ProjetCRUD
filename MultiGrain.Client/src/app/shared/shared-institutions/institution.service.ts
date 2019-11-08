aeaf326097dcf97e45187923f5cb2832dc0f9737
Sassine Jaoude <sassine.jaoude@hotmail.com>
2019-11-08 08:42:17 +0200

    Changed the http address of the institution service to https

--- MultiGrain.Client/src/app/shared/shared-institutions/institution.service.ts
+++ MultiGrain.Client/src/app/shared/shared-institutions/institution.service.ts
@@ -1,32 +1,36 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
-import { Institution } from './institution.model';
+import { Institution } from "./institution.model";
 
+const newLocal = "/Institutions";
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class InstitutionService {
-
   formData: Institution;
-  //API needed from backend for Institutions 
-  readonly rootURL = 'http://localhost:59035/api';
-  list : Institution[];
+  //API needed from backend for Institutions
+  readonly rootURL = "https://localhost:44368/api";
+  list: Institution[];
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   postInstitution() {
-    return this.http.post(this.rootURL + '/institutions', this.formData);
+    return this.http.post(this.rootURL + "/institutions", this.formData);
   }
   putInstitution() {
-    return this.http.put(this.rootURL + '/Institutions/'+ this.formData.InstId, this.formData);
+    return this.http.put(
+      this.rootURL + "/Institutions/" + this.formData.InstId,
+      this.formData
+    );
   }
   deleteInstitution(id) {
-    return this.http.delete(this.rootURL + '/Institutions/'+ id);
+    return this.http.delete(this.rootURL + "/Institutions/" + id);
   }
 
-  refreshList(){
-    this.http.get(this.rootURL + '/Institutions')
-    .toPromise()
-    .then(res => this.list = res as Institution[]);
+  refreshList() {
+    this.http
+      .get(this.rootURL + newLocal)
+      .toPromise()
+      .then(res => (this.list = res as Institution[]));
   }
 }
