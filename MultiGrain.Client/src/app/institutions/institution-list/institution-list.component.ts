e0321b97f00c7156d0cc30a507d657914d66238c
Sassine Jaoude <sassine.jaoude@hotmail.com>
2019-11-24 21:44:21 +0200

    Update - fix for the LectureDuration not showing

--- MultiGrain.Client/src/app/institutions/institution-list/institution-list.component.ts
+++ MultiGrain.Client/src/app/institutions/institution-list/institution-list.component.ts
@@ -1,40 +1,43 @@
-import { Component, OnInit } from '@angular/core';
-import { ToastrService } from 'ngx-toastr';
-import { Institution } from './../../shared/shared-institutions/institution.model';
-import { InstitutionService } from './../../shared/shared-institutions/institution.service';
+import { Component, OnInit } from "@angular/core";
+import { ToastrService } from "ngx-toastr";
+import { Institution } from "./../../shared/shared-institutions/institution.model";
+import { InstitutionService } from "./../../shared/shared-institutions/institution.service";
 
 @Component({
-  selector: 'app-institution-list',
-  templateUrl: './institution-list.component.html',
+  selector: "app-institution-list",
+  templateUrl: "./institution-list.component.html",
   styles: []
 })
 export class InstitutionListComponent implements OnInit {
-
-  constructor(private service: InstitutionService,
-    private toastr: ToastrService) { }
+  constructor(
+    private service: InstitutionService,
+    private toastr: ToastrService
+  ) {
+    alert();
+  }
 
   ngOnInit() {
-    
     this.service.refreshList();
   }
   populateForm(pd: Institution) {
-  debugger;
-   this.service.formData = Object.assign({}, pd);
+    debugger;
+
+    this.service.formData = Object.assign({}, pd);
   }
 
   onDelete(InstId) {
-    if (confirm('Are you sure to delete this record ?')) {
-      this.service.deleteInstitution(InstId)
-        .subscribe(res => {
+    if (confirm("Are you sure to delete this record ?")) {
+      this.service.deleteInstitution(InstId).subscribe(
+        res => {
           debugger;
           this.service.refreshList();
-          this.toastr.warning('Deleted successfully', 'Institution Register');
+          this.toastr.warning("Deleted successfully", "Institution Register");
         },
-          err => {
-            debugger;
-            console.log(err);
-          })
+        err => {
+          debugger;
+          console.log(err);
+        }
+      );
     }
   }
-
 }
